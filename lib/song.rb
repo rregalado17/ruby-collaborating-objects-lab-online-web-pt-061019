02301960276099ad8296d03f1b13e8713789ca11
Robinson Regalado <rregalado17@gmail.com>
2019-07-24 03:12:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,14 @@
-require "pry"
+class Song
+  attr_accessor :name, :artist
 
-class Song 
-  attr_accessor :name, :artist 
-  
   def initialize(name)
-    @name = name 
+    @name = name
   end
-  
+
   def self.new_by_filename(filename)
-    song_name = filename.split(" - ")[1]
-    song = Song.new(song_name)
-    artist_object = Artist.find_or_create_by_name(filename.split(" - ")[0])
-    song.artist = artist_object
-    song.artist.add_song(self)
-    song
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
   end
-  
 end
