15622919487561b22bbd66487ad8067520f1f41d
Robinson Regalado <rregalado17@gmail.com>
2019-07-24 03:13:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,35 @@
-class Artist
-  attr_accessor :name
-
+class Artist 
+  attr_accessor :name, :songs
+  
   @@all = []
-
+  
   def initialize(name)
-    @name = name
-    @songs= []
+    @name = name 
+    @songs = []
+    self.save 
   end
-
+  
   def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def songs
-    @songs
-  end
-
-  def save
-    @@all << self
-    self
+    @songs << song 
   end
 
   def self.all
-    @@all
+    @@all 
   end
 
+  def save 
+    self.class.all << self 
+  end 
+    
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+    if self.all.find {|artist| artist.name == name}
+    else
+      self.new(name)
+    end
   end
-
+    
   def print_songs
-    self.songs.each {|song| puts song.name} 
-   end
+    self.songs.each {|song| puts song.name}
+  end
+    
 end
