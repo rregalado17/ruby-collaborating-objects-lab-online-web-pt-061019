98144f40d99c0344595b58fdd670d5f2830d6573
Robinson Regalado <rregalado17@gmail.com>
2019-07-24 03:18:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     artist, title = filename.split(" - ")
     self.new(title).tap {|song|}
       song.artist = Artist.find_or_create_by_name(artist)
-      song.artist.add_song(song)}
+      song.artist.add_song(song)
   end
   
 end
