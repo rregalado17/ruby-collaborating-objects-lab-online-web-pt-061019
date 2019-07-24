c840b112c8867d1d4e72c75a78a12a5d4ce956cb
Robinson Regalado <rregalado17@gmail.com>
2019-07-24 03:10:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,8 @@ class Song
   def self.new_by_filename(filename)
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_
+    or_create_by_name(artist_name)
     artist.add_song(song)
   end
   
