17a101cfc89dfe5d99bf006fb4c166cf17c0519f
Robinson Regalado <rregalado17@gmail.com>
2019-07-24 03:22:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,9 +22,7 @@ class Artist
   end 
     
   def self.find_or_create_by_name(name)
-    if self.all.find {|artist| artist.name == name}
-    else
-      self.new(name)
+    self.all.find {|artist| artist.name == name} || self.new(name)
     end
   end
     
